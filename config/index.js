494fae03f747056360549634ba9a1be437a5298b
tianhukj <166341634+tianhukj@users.noreply.github.com>
2025-01-19 07:56:02 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,13 +6,13 @@ const CONFIG = {
   // ================================高级配置 - 需要修改================================
 
   // 微信公众号的appID
-  APP_ID: "wx8cce380b885eda7b",
+  APP_ID: "wx3e5290f51659ebd0",
 
   // 微信公众号的appsecret
-  APP_SECRET: "ed4be85c5b4c9ec20ff6a4573c1c0b9e",
+  APP_SECRET: "33538d4e786ad10dd0da9cea1c79ca57",
 
   // 微信公众号的模板ID
-  TEMPLATE_ID: "hHFo2kk59WI7Ef8R2Ry0_IMQWbUlWIPLfJd1lIkMkuo",
+  TEMPLATE_ID: "rdlKXnh-mlQGnwmV7wXKSXRnI9-nZe_qXuJhe3VCeAo",
 
   // 天行API的key
   TXApiKey: "96fd738b99e30ffa1014dad845cd0764",
@@ -21,23 +21,18 @@ const CONFIG = {
   user: [
     {
       // 男(女)朋友的名字（或昵称或网名）
-      userName: "小沐沐吖",
+      userName: "小颉颉吖",
       // 用户列表的`微信号`
-      userId: "ouvDW5kPNhzombB3FdIeahEeM6CI",
+      userId: "ovvpK7AjmE8fA3lodi0uJVrVjxL8",
       // 星座
-      star: "摩羯座",
+      star: "水瓶座",
       // 生日 - 格式：08-22
-      birthday: "01-28",
+      birthday: "02-05",
       // 城市 - 格式：支持省市县区
-      city: "夏邑县",
+      city: "东营市",
     },
   ],
 
-  // 计划旅游日 - 格式：2022-05-20
-  tourism: "2022-12-12",
-
-  // 恋爱纪念日 - 格式：2022-05-20
-  loveDay: "2022-05-20",
 
   // ================================功能配置 - 可选修改================================
 
